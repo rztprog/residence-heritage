562d49fae1da99da91693c50e06188b981efdc68
rztprog <rztprog@outlook.fr>
2025-07-05 18:44:30 +0200

    Add flat controller and view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -44,7 +44,7 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :title, :description, :main_photo, :rent, :charges)
   end
 
   def set_flat

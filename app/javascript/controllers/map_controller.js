64c0285f196deb2879bdd63605533830f5c8190d
rztprog <rztprog@outlook.fr>
2025-07-14 18:09:29 +0200

    Add mapbox map, and mapbox css tttto layout, custom mapbox map css

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -6,24 +6,52 @@ export default class extends Controller {
         const token = document.querySelector('meta[name="mapbox-token"]').content
         mapboxgl.accessToken = token
 
+        // Real values
         const latitude = parseFloat(this.data.get("latitude"))
         const longitude = parseFloat(this.data.get("longitude"))
-        const name = this.data.get("name");
+        const name = this.data.get("name")
+        const address = this.data.get("address")
 
-        const map = new mapboxgl.Map({
+        // Fallback values
+        const isValidCoords = !isNaN(latitude) && !isNaN(longitude)
+        const fallbackCoords = [3.057256, 50.62925] // Lille
+        const center = isValidCoords ? [longitude, latitude] : fallbackCoords
+
+        this.map = new mapboxgl.Map({
             container: this.element,
             style: 'mapbox://styles/mapbox/streets-v12',
-            center: [longitude, latitude],
-            zoom: 14
+            center: center,
+            zoom: 15
         })
 
-        new mapboxgl.Marker()
-            .setLngLat([longitude, latitude])
-            .setPopup(new mapboxgl.Popup().setText(name))
-            .addTo(map)
+        // Add marker if valid coords
+        if (isValidCoords) {
+            new mapboxgl.Marker()
+                .setLngLat([longitude, latitude])
+                .setPopup(new mapboxgl.Popup({ offset: 25, closeButton: true, closeOnClick: true }).setHTML(`
+                    <div class="custom-popup">
+                        <h3><b>${name}</b></h3>
+                        <p>${address}</p>
+                    </div>
+                `))
+                .addTo(this.map)
+        }
 
-        map.addControl(new mapboxgl.NavigationControl());
-        map.scrollZoom.disable();
+        this.map.scrollZoom.disable();
+        this.map.addControl(new mapboxgl.NavigationControl());
+
+        // Force resize sometimes
+        this.map.on("load", () => {
+            this.map.resize()
+        })
+    }
+
+    disconnect() {
+        if (this.map) {
+            this.map.remove()
+            this.map = null
+        }
     }
 }
+
 

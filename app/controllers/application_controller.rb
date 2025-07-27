4b34240ae7ef4f2e5b1859f65450238ee96a33f8
rztprog <rztprog@outlook.fr>
2025-07-27 16:51:07 +0200

    Add flat (limit 5) on footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :load_footer_flats
+
+  def load_footer_flats
+    @footer_flats = Flat.limit(5)
+  end
 end

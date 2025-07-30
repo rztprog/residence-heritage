5e86750448ed4ce491c81fe7af95ca2a0f64ee19
rztprog <rztprog@outlook.fr>
2025-07-30 23:27:04 +0200

    Add main_photo_validation to flat

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,5 @@ class Flat < ApplicationRecord
   has_one_attached :main_photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  validate :main_photo_presence
 end
